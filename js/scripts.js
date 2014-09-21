073212f2dcc7c5cd4dc4789e403f147f51475623
Kevin McGillivray <kevin.mcgillivray@me.com>
2014-09-20 23:28:24 -0500

    Add sidebar to resume.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,26 @@ var main = function() {
     $('.nav-menu').slideToggle(300);
     $('.menu-button').toggleClass('fa-bars').toggleClass('fa-times');
   });
+  
+  $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+      
+      if (scroll >= 100) {
+        $(".sidebar").addClass("sidebar-scroll");
+        $(".main").addClass("main-scroll");
+      } else {
+        $(".sidebar").removeClass("sidebar-scroll");
+        $(".main").removeClass("main-scroll");
+      }
+  });
+  
+  var offset = 20;
+
+  $('.sidebar-nav li a').click(function(event) {
+      event.preventDefault();
+      $($(this).attr('href'))[0].scrollIntoView();
+      scrollBy(0, -offset);
+  });
 }
 
 $(document).ready(main);
