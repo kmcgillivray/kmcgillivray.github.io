90b418db93abbcf65b6b28a3a71535fda8cf3227
Kevin McGillivray <kevin.mcgillivray@me.com>
2016-12-07 23:04:48 -0600

    Delete old scripts

--- js/status-checker.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("statusChecker", ["firebase"]);
-
-app.controller("MainController", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://status-checker.firebaseio.com/projects");
-  var syncObject = $firebaseObject(ref);
-  syncObject.$bindTo($scope, "projects");
-});
