10d364bfaca98d1b3a297e784009b26992de63c2
Kevin McGillivray <kevin.mcgillivray@me.com>
2019-01-20 22:23:00 -0600

    Switch publication check to Airtable

--- _src/functions/deploy-succeeded.js
+++ _src/functions/deploy-succeeded.js
@@ -1,11 +1,15 @@
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import Twitter from 'twitter';
+import Airtable from 'airtable';
 import { AllHtmlEntities as Entities } from 'html-entities';
 
 dotenv.config();
 
 const NOTES_URL = 'https://kevinmcgillivray.net/notes.json';
+const BASE_ID = 'app6OkG5p6ec08fZg';
+
+const base = new Airtable().base(BASE_ID);
 
 const twitter = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -15,7 +19,7 @@ const twitter = new Twitter({
 });
 
 const prepareStatusText = note => {
-  const maxLength = 280 - 41;
+  const maxLength = 280;
   const entities = new Entities();
 
   // strip html tags and decode entities
@@ -28,57 +32,63 @@ const prepareStatusText = note => {
   }
 
   // include the note url at the end;
-  text = text + ' ' + note.url;
+  // text = text + ' ' + note.url;
   return text;
+}
+
+const saveNoteToAirtable = async note => {
+  base('Notes').create({
+    "ID": parseInt(note.id),
+    "URL": note.url
+  }, function(err, record) {
+    if (err) { return handleError(err); }
+    console.log('Note successfully saved to Airtable');
+  });
 }
 
 const publishNote = async note => {
   try {
-    const statusText = prepareStatusText(note)
+    const statusText = prepareStatusText(note);
     const tweet = await twitter.post('statuses/update', {
       status: statusText
-    })
+    });
+    const airtableRecord = await saveNoteToAirtable(note);
     if (tweet) {
-      return status(
-        200,
-        `Note ${note.date} successfully posted to Twitter.`
-      )
+      return status(200, `Note ${note.date} successfully posted to Twitter.`);
     } else {
-      return status(422, 'Error posting to Twitter API.')
+      return status(422, 'Error posting to Twitter API.');
     }
   } catch (err) {
-    return handleError(err)
+    return handleError(err);
   }
 }
 
 const processNotes = async notes => {
   if (!notes.length) {
-    return status(404, 'No notes found to process.')
+    return status(404, 'No notes found to process.');
   }
 
   const latestNote = notes[0];
   if (!latestNote.syndicate) {
-    return status(
-      400,
-      'Latest note has disabled syndication. No action taken.'
-    )
+    return status(400, 'Latest note has disabled syndication. No action taken.');
   }
 
   try {
-    // check twitter for any tweets containing note URL.
-    // if there are none, publish it.
-    const q = await twitter.get('search/tweets', { q: latestNote.url });
-    if (q.statuses && q.statuses.length === 0) {
+    // Check Airtable for a record with the ID and only syndicate if it doesn't exist in Airtable
+    const records = await base('Notes').select({
+      maxRecords: 1,
+      filterByFormula: `{ID} = ${latestNote.id}`
+    }).firstPage();
+
+    if (records && records.length === 0) {
       return publishNote(latestNote);
     } else {
-      return status(
-        400,
-        'Latest note was already syndicated. No action taken.'
-      )
+      return status(400, 'Latest note was already syndicated. No action taken.');
     }
   } catch (err) {
     return handleError(err);
   }
+
 }
 
 const handleError = err => ({
